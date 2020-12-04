a6797bfef5532015fdf5d30c68ed062eae792734
Isabella Rhee <ibella214@Isabellas-Air.lan>
2020-12-04 12:26:44 -0500

    December3

--- temp.py
+++ temp.py
@@ -1,18 +1,14 @@
+def getCellCoordinates(row = 19, col = 10):
+    rows, cols = 20, 20
+    cellSize = 500
+    offsetX = -1*(500*(cols//2))
+    offsetY = -1*(500*(rows-1))
+    print(offsetX, offsetY)
+    x0 = col * 500 + offsetX
+    y1 = row * 500 + offsetY
+    
+    x = x0 + int(.5*cellSize)
+    y = y1 + int(.5*cellSize)
+    print(x, y)
 
-
-
-def timerFired(mode):
-    mode.x1 = random.randint(20, mode.width)
-    mode.y1 = randomg.randint(20, mode.height)
-    mode.x2 = mode.x1 + 70
-    mode.y2 = mode.y1 + 12
-    newPlatform = (mode.x1, mode.y1, mode.x2, mode.y2)
-    if len(mode.platforms) < 10:
-        if mode.checkPlatform(newPlatform):
-            mode.platforms.append(newPlatform)
-
-def checkPlatform(mode, newPlatform):
-    if (within the bounds):
-        return True
-    else:
-        return False
+getCellCoordinates()
